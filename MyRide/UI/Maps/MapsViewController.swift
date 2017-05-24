bea5d358733e7a686812dd2d0493d410d39db176
Flavien SICARD <flavien.sicard@gmail.com>
2017-05-24 17:49:53 +0200

    feat(SideMenu): Add history

--- MyRide/UI/Maps/MapsViewController.swift
+++ MyRide/UI/Maps/MapsViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Mapbox
 import SwiftyJSON
+import CoreData
 
 class MapsViewController: UIViewController, SearchAddressDelegate, MapDelegate {
 
@@ -18,8 +19,11 @@ class MapsViewController: UIViewController, SearchAddressDelegate, MapDelegate {
     
     private let sideMenuController = SideMenuViewController()
     
+    var history: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setup()
     }
 
@@ -44,12 +48,39 @@ class MapsViewController: UIViewController, SearchAddressDelegate, MapDelegate {
         self.sideMenuController.view.frame = CGRect(x: -self.view.frame.size.width, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
         view.addSubview(self.sideMenuController.view)
         self.sideMenuController.didMove(toParentViewController: self)
+        self.sideMenuController.mapsViewController = self
     }
     
     private func alertView(message: String) {
         let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func saveHistory(address: String, lat: String, lng: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.managedObjectContext
+        let entity = NSEntityDescription.entity(forEntityName: "History", in: managedContext)!
+        let ride = NSManagedObject(entity: entity, insertInto: managedContext)
+        ride.setValue(address, forKeyPath: "address")
+        ride.setValue(lat, forKeyPath: "lat")
+        ride.setValue(lng, forKeyPath: "lng")
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "History")
+        
+        do {
+            history = try managedContext.fetch(fetchRequest)
+            if history.count == 16 {
+                appDelegate.managedObjectContext.delete(history[0])
+            }
+            try managedContext.save()
+            history.append(ride)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+
     }
     
     // MARK : SearchAddressDelegate
@@ -72,6 +103,7 @@ class MapsViewController: UIViewController, SearchAddressDelegate, MapDelegate {
                 let json = JSON(data!)
                 let lat = json["results"][0]["geometry"]["location"]["lat"].doubleValue
                 let lng = json["results"][0]["geometry"]["location"]["lng"].doubleValue
+                self.saveHistory(address: address, lat: String(lat), lng: String(lng))
                 self.mapboxView.centerCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lng)
                 self.mapboxView.pinIsHidden = false
             } else {
@@ -93,11 +125,17 @@ class MapsViewController: UIViewController, SearchAddressDelegate, MapDelegate {
             if success {
                 let json = JSON(data!)
                 self.searchAddressView.address = json["results"][0]["formatted_address"].stringValue
+                self.saveHistory(address: self.searchAddressView.address, lat: String(coordinate.latitude), lng: String(coordinate.longitude))
             } else {
                 self.searchAddressView.address = addressTmp
                 self.alertView(message: error!.localizedDescription as String)
             }
         }
+    }
+    
+    func updateLocation(address: String, lat: Double, lng: Double) {
+        searchAddressView.address = address
+        self.mapboxView.centerCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lng)
     }
 
 }

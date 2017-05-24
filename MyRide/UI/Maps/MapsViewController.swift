991f05a103afce15c663c26c537fc80f2a1da641
Flavien SICARD <flavien.sicard@gmail.com>
2017-05-24 09:18:45 +0200

    Add project + Cocoapods

---
+++ MyRide/UI/Maps/MapsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapsViewController.swift
+//  MyRide
+//
+//  Created by Flavien SICARD on 24/05/2017.
+//  Copyright © 2017 sicardf. All rights reserved.
+//
+
+import UIKit
+
+class MapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MyRide' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Mapbox-iOS-SDK', '~> 3.5'
+  # Pods for MyRide
+
+  target 'MyRideTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end

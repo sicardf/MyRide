586763ad099aec813ef5662d37a75570b8e5adf1
Flavien SICARD <flavien.sicard@gmail.com>
2017-05-24 14:06:30 +0200

    ETAPE 3

--- MyRide/UI/Maps/MapsViewController.swift
+++ MyRide/UI/Maps/MapsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Mapbox
 import SwiftyJSON
 
-class MapsViewController: UIViewController, SearchAddressDelegate {
+class MapsViewController: UIViewController, SearchAddressDelegate, MapDelegate {
 
     private var mapboxView: MapboxView!
     private var searchAddressView: SearchAddressView!
@@ -27,6 +27,7 @@ class MapsViewController: UIViewController, SearchAddressDelegate {
     
     private func setup() {
         mapboxView = MapboxView(frame: view.frame)
+        mapboxView.delegate = self
         view.addSubview(mapboxView)
         
         searchAddressView = SearchAddressView(frame: CGRect(x: 20, y: 30, width: self.view.frame.size.width - 40, height: 250))
@@ -54,15 +55,31 @@ class MapsViewController: UIViewController, SearchAddressDelegate {
     }
     
     func getAddressMap(address: String) {
-        apiGoogle.getGeocodeLatlng(address: address) { (success, data, error) in
+        apiGoogle.getGeocodeAddress(address: address) { (success, data, error) in
             self.view.endEditing(true)
             if success {
                 let json = JSON(data!)
                 let lat = json["results"][0]["geometry"]["location"]["lat"].doubleValue
                 let lng = json["results"][0]["geometry"]["location"]["lng"].doubleValue
                 self.mapboxView.centerCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lng)
-                self.mapboxView.addPin()
+                self.mapboxView.pinIsHidden = false
             } else {
+                self.alertView(message: error!.localizedDescription as String)
+            }
+        }
+    }
+    
+    // MARK : MapDelegate
+    
+    func centerCoordinateMapChange(coordinate: CLLocationCoordinate2D) {
+        let addressTmp = self.searchAddressView.address
+        self.searchAddressView.address = "Update of location ..."
+        apiGoogle.getGeocodeLatlng(lat: String(coordinate.latitude), lng: String(coordinate.longitude)) { (success, data, error) in
+            if success {
+                let json = JSON(data!)
+                self.searchAddressView.address = json["results"][0]["formatted_address"].stringValue
+            } else {
+                self.searchAddressView.address = addressTmp
                 self.alertView(message: error!.localizedDescription as String)
             }
         }
